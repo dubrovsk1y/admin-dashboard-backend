1bffb0d76884bfd03121cc862dc9a67d236a6e52
Yuriy Dubrovskiy <y.dubrovskiy@syberry.com>
2023-02-25 19:00:52 +0100

    Add basic configuration

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import router from "./router/index.js";
+
+// CONFIGURATION
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/api", router);
+
+const PORT = process.env.PORT || 9090;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`SERVER STARTED ON PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
